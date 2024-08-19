442cee77de43d9ed8429666ad639db128d705d41
Pedro Ferraiuoli <92264274+pedroferraiuoli@users.noreply.github.com>
2024-08-19 12:26:35 -0300

    Update experience.js

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,16 +1,16 @@
 // src/data/experiences.js
 export const experiences = [
     {
-      title: 'Analista de Redes',
-      company: 'IFF - Instituto Federal Fluminense',
-      date: 'Jan/2023 - Dez/2023',
-      description: 'Trabalhei como analista de redes, gerenciando e mantendo a infraestrutura de rede.',
+      title: 'Desenvolvedor Full-Stack',
+      company: 'Prefeitura de Campos dos Goytacazes - Estágio',
+      date: 'dez/2023 - Atualmente',
+      description: 'Desenvolvimento sites e ferramentas usando Django, HTML/CSS e JS.',
     },
     {
-      title: 'Desenvolvedor Backend',
-      company: 'Freelancer',
-      date: 'Jan/2020 - Atualmente',
-      description: 'Desenvolvimento de soluções backend para diversos projetos, utilizando Node.js e Python.',
+      title: 'Analise e gestão de dados',
+      company: 'Prefeitura de Campos dos Goytacazes - Estágio',
+      date: 'dez/2023 - Atualmente',
+      description: 'Ajudo a criar e manter relatórios feitos no Power BI.',
     },
   ];
   
