aad85c0a38c4104676d4e802926efcb60ccd4e5c
pedroferraiuoli <pedrofpteixeira4@gmail.com>
2024-08-22 14:01:49 -0300

    integração mobile

--- src/App.js
+++ src/App.js
@@ -11,42 +11,42 @@ import { MdOutlineWorkOutline } from "react-icons/md";
 
 const App = () => {
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row">
       <Sidebar />
-      <div className="flex-1 bg-gray-800 text-white p-10">
-        <section id="home" className="mb-20 flex gap-20 items-center justify-center">
-        <img src="/perfil.jpg"  className=" w-36 rounded border-2 border-gray-800"></img>
+      <div className="flex-1 bg-gray-800 text-white p-4 md:p-10">
+        <section id="home" className="mb-10 md:mb-20 flex flex-col md:flex-row gap-6 md:gap-20 items-center justify-center">
+          <img src="/perfil.jpg" className="w-24 md:w-36 rounded border-2 border-gray-800"></img>
 
           <div id="introducao">
-          <h1 className="text-4xl font-bold mb-4">Pedro Ferraiuoli</h1>
-          <p className="text-lg">Sou estudante de sistemas e desenvolvedor! Conheça um pouco do meu trabalho.</p>
+            <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">Pedro Ferraiuoli</h1>
+            <p className="text-sm md:text-lg">Sou estudante de sistemas e desenvolvedor! Conheça um pouco do meu trabalho.</p>
 
-          <div className="mt-2 flex flex-wrap gap-2">
-            <span className="bg-gray-600 text-sm px-2 py-1 rounded flex items-center gap-1">
-              Python <div><MdOutlineWorkOutline /></div>
-            </span>
-            <span className="bg-gray-600 text-sm px-2 py-1 rounded flex items-center gap-1">
-              Django <div><MdOutlineWorkOutline /></div>
-            </span>
-            <span className="bg-gray-600 text-sm px-2 py-1 rounded flex items-center gap-1">
-              HTML/CSS <div><MdOutlineWorkOutline /></div>
-            </span>
-            <span className="bg-gray-600 text-sm px-2 py-1 rounded flex items-center gap-1">
-              Power BI <div><MdOutlineWorkOutline /></div>
-            </span>
-            <span className="bg-gray-600 text-sm px-2 py-1 rounded flex items-center gap-1">
-              Java <div><HiOutlineAcademicCap /></div>
-            </span>
-            <span className="bg-gray-600 text-sm px-2 py-1 rounded flex items-center gap-1">
-              MySql <div><HiOutlineAcademicCap /></div>
-            </span>
-        </div>
-
+            <div className="mt-2 flex flex-wrap gap-2">
+              <span className="bg-gray-600 text-xs md:text-sm px-2 py-1 rounded flex items-center gap-1">
+                Python <div><MdOutlineWorkOutline /></div>
+              </span>
+              <span className="bg-gray-600 text-xs md:text-sm px-2 py-1 rounded flex items-center gap-1">
+                Django <div><MdOutlineWorkOutline /></div>
+              </span>
+              <span className="bg-gray-600 text-xs md:text-sm px-2 py-1 rounded flex items-center gap-1">
+                HTML/CSS <div><MdOutlineWorkOutline /></div>
+              </span>
+              <span className="bg-gray-600 text-xs md:text-sm px-2 py-1 rounded flex items-center gap-1">
+                Power BI <div><MdOutlineWorkOutline /></div>
+              </span>
+              <span className="bg-gray-600 text-xs md:text-sm px-2 py-1 rounded flex items-center gap-1">
+                Java <div><HiOutlineAcademicCap /></div>
+              </span>
+              <span className="bg-gray-600 text-xs md:text-sm px-2 py-1 rounded flex items-center gap-1">
+                MySql <div><HiOutlineAcademicCap /></div>
+              </span>
+            </div>
           </div>
         </section>
-        <section id="about" className="mb-20">
-          <h2 className="text-3xl font-semibold mb-4">Sobre Mim</h2>
-          <p className="">Sou estudante de Sistemas de Informação, atualmente no quinto período, com uma sólida base em várias áreas da tecnologia e desenvolvimento de software. Meu percurso acadêmico inclui cursos em Java e MySQL, que fortaleceram minha compreensão dos fundamentos da programação e gestão de banco de dados.
+
+        <section id="about" className="mb-10 md:mb-20">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4">Sobre Mim</h2>
+          <p className="text-sm md:text-base">Sou estudante de Sistemas de Informação, atualmente no quinto período, com uma sólida base em várias áreas da tecnologia e desenvolvimento de software. Meu percurso acadêmico inclui cursos em Java e MySQL, que fortaleceram minha compreensão dos fundamentos da programação e gestão de banco de dados.
 
 Atualmente, trabalho na prefeitura da minha cidade como analista e gestor de dados e desenvolvedor. Nesse papel, sou responsável por analisar e gerenciar dados, garantindo a integridade e a acessibilidade das informações para suportar a tomada de decisões. Além disso, desenvolvo soluções de software utilizando Python e Django, o que me permite criar aplicações web robustas e eficientes.
 
@@ -54,25 +54,28 @@ Tenho experiência com Power BI, que utilizo para criar relatórios e dashboards
 
 Estou sempre buscando aprender e me adaptar às novas tecnologias e metodologias para aprimorar minhas habilidades e entregar soluções inovadoras e eficazes.</p>
         </section>
-        <section id="experience" className="mb-20">
-          <h2 className="text-3xl font-semibold mb-4">Experiências Recentes</h2>
-          <div className="grid grid-cols-2 gap-6">
+
+        <section id="experience" className="mb-10 md:mb-20">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4">Experiências Recentes</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
             {experiences.map((exp, index) => (
               <ExperienceCard key={index} {...exp} />
             ))}
           </div>
         </section>
-        <section id="course" className="mb-20">
-          <h2 className="text-3xl font-semibold mb-4">Cursos realizados</h2>
-          <div className="grid grid-cols-2 gap-6">
+
+        <section id="course" className="mb-10 md:mb-20">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4">Cursos realizados</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
             {courses.map((exp, index) => (
               <ExperienceCard key={index} {...exp} />
             ))}
           </div>
         </section>
+
         <section id="projects">
-          <h2 className="text-3xl font-semibold mb-4">Projetos</h2>
-          <div className="grid grid-cols-2 gap-6">
+          <h2 className="text-2xl md:text-3xl font-semibold mb-4">Projetos</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
             {projects.map((project, index) => (
               <ProjectCard key={index} {...project} />
             ))}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,45 +1,71 @@
-import React from 'react';
-import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope, FaDownload } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaGithub, FaLinkedin, FaDownload, FaBars, FaTimes } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <aside className="w-50 bg-gray-800 text-white p-4 border-r border-gray-700">
-      <h1 className="text-2xl font-bold mb-6">Meu portfólio</h1>
-      <nav>
-        <ul className="space-y-4">
-          <li>
-            <a href="#home" className="hover:underline flex border border-gray-600 p-2 rounded place-content-center">Home</a>
-          </li>
-          <li>
-            <a href="#projects" className="hover:underline border border-gray-600 p-2 rounded place-content-center flex">Projetos</a>
-          </li>
-          <li>
-            <a href="#about" className="hover:underline border border-gray-600 p-2 rounded place-content-center flex" >Sobre Mim</a>
-          </li>
-        
-        </ul>
-      </nav>
-      <div className="mt-8">
-        <h2 className="text-xl mb-4">Socials</h2>
-        <ul className="space-y-4">
-          <li>
-            <a href="https://github.com/pedroferraiuoli" target="_blank" className="flex items-center border border-gray-600 p-2 rounded">
-              <FaGithub className="mr-2" /> Github
-            </a>
-          </li>
-          <li>
-            <a href="https://linkedin.com/in/pedro-ferraiuoli-283568296" className="flex items-center border border-gray-600 p-2 rounded">
-              <FaLinkedin className="mr-2" /> LinkedIn
-            </a>
-          </li>
-          <li>
-            <a href="/Currículo.pdf" download className="flex items-center border border-gray-600 p-2 rounded">
-              <FaDownload className="mr-2" /> Currículo
-            </a>
-          </li>
-        </ul>
+    <div>
+      {!isOpen && (
+      <button onClick={toggleMenu} className="fixed top-4 left-4 z-50 text-white">
+      <FaBars size={24} /> 
+      </button>
+       )}
+
+      <aside
+        className={`fixed top-0 left-0 h-full w-64 bg-gray-800 text-white p-4 border-r border-gray-700 transform ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        } transition-transform duration-300 ease-in-out z-40`}
+      >
+        <button
+          onClick={toggleMenu}
+          className="absolute top-5 right-4 text-white"
+        >
+          <AiOutlineClose size={24} />
+        </button>
+        <h1 className="text-2xl font-bold mb-6">Meu portfólio</h1>
+        <nav>
+          <ul className="space-y-4">
+            <li>
+              <a href="#home" className="hover:underline flex border border-gray-600 p-2 rounded place-content-center">Home</a>
+            </li>
+            <li>
+              <a href="#projects" className="hover:underline border border-gray-600 p-2 rounded place-content-center flex">Projetos</a>
+            </li>
+            <li>
+              <a href="#about" className="hover:underline border border-gray-600 p-2 rounded place-content-center flex">Sobre Mim</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="mt-8">
+          <h2 className="text-xl mb-4">Socials</h2>
+          <ul className="space-y-4">
+            <li>
+              <a href="https://github.com/pedroferraiuoli" target="_blank" className="flex items-center border border-gray-600 p-2 rounded">
+                <FaGithub className="mr-2" /> Github
+              </a>
+            </li>
+            <li>
+              <a href="https://linkedin.com/in/pedro-ferraiuoli-283568296" className="flex items-center border border-gray-600 p-2 rounded">
+                <FaLinkedin className="mr-2" /> LinkedIn
+              </a>
+            </li>
+            <li>
+              <a href="/Currículo.pdf" download className="flex items-center border border-gray-600 p-2 rounded">
+                <FaDownload className="mr-2" /> Currículo
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
       </div>
-    </aside>
+      
+
   );
 };
 
